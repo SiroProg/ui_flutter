5eb0a4aafe308b88e3e9601b605a4c9289aa51d5
SiroProg <070777sss@gmail.com>
2023-08-12 00:03:56 +0500

    Grid_6

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,16 @@ void main() {
                 ),
               ),
             ),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(right: 10, left: 10, bottom: 10),
+                width: 210,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade200,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+              ),
+            ),
           ],
         ),
       ),
