3f46a51ec29e7bb6821507f09802f153a17ae285
SiroProg <070777sss@gmail.com>
2023-08-11 22:46:30 +0500

    3_UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,65 +4,91 @@ void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Container(
-        color: Colors.white,
-        child: Container(
-          margin: EdgeInsets.all(5),
+      home: Scaffold(
+        body: Container(
+          margin: const EdgeInsets.all(15),
+          padding: EdgeInsets.all(10),
           decoration: BoxDecoration(
-            color: Colors.blue.shade800,
             border: Border.all(
-              color: const Color.fromARGB(255, 9, 55, 133),
+              color: Colors.blue.shade900,
               width: 10,
             ),
           ),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Container(
-                height: 50,
-                margin: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.green.shade800,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 5,
-                  ),
+              Text(
+                'Column',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 17, 36, 203),
                 ),
               ),
               Container(
-                height: 50,
-                margin: EdgeInsets.all(10),
+                margin: EdgeInsets.only(top: 10),
+                padding: EdgeInsets.only(top: 6, left: 14),
+                height: 150,
                 decoration: BoxDecoration(
-                  color: Colors.green.shade800,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 5,
+                  border: Border.all(color: Colors.black, width: 10),
+                ),
+                child: Text('Fixed height container'),
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  margin: EdgeInsets.only(top: 10),
+                  height: 438,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: Color.fromARGB(255, 88, 8, 164), width: 10),
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Row',
+                              style: TextStyle(
+                                color: Colors.deepPurple.shade900,
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                margin: EdgeInsets.only(top: 10, right: 10),
+                                padding: EdgeInsets.all(20),
+                                alignment: Alignment.centerLeft,
+                                child: Text(
+                                  'Expanded \nchart',
+                                  style: TextStyle(color: Colors.red.shade900),
+                                ),
+                                // width: 225,
+                                // height: 375,
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.red.shade900, width: 10)),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.only(top: 10),
+                          child: Text('Fixed\nwidth\ncontainer'),
+                          width: 115,
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.black, width: 10)),
+                        ),
+                      )
+                    ],
                   ),
                 ),
-              ),
-              Container(
-                height: 50,
-                margin: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.green.shade800,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 5,
-                  ),
-                ),
-              ),
-              Container(
-                height: 50,
-                margin: EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Colors.green.shade800,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 5,
-                  ),
-                ),
-              ),
+              )
             ],
           ),
         ),
