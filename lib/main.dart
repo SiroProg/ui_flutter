86416e3fb76c14452e75ef84f078a7312ec38528
SiroProg <070777sss@gmail.com>
2023-08-11 23:55:53 +0500

    Grid_2

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,36 @@ void main() {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Container(
-              margin: EdgeInsets.all(10),
-              width: 215,
-              decoration: BoxDecoration(
-                color: Colors.blue.shade200,
-                borderRadius: BorderRadius.all(Radius.circular(20)),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.all(10),
+                width: 215,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade200,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
               ),
             ),
-            Container(
-              margin: EdgeInsets.only(right: 10, top: 10, bottom: 10),
-              width: 210,
-              decoration: BoxDecoration(
-                color: Colors.blue.shade200,
-                borderRadius: BorderRadius.all(Radius.circular(20)),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(right: 10, top: 10, bottom: 10),
+                width: 210,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade200,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
               ),
             ),
-            
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(right: 10, top: 10, bottom: 10),
+                width: 210,
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade200,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+              ),
+            ),
           ],
         ),
       ),
