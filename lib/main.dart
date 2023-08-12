f3624cd70eb697dcbb20e32d2eae41fd4589ffac
SiroProg <070777sss@gmail.com>
2023-08-12 13:37:21 +0500

    Grid_15

--- lib/main.dart
+++ lib/main.dart
@@ -5,77 +5,49 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-          Row(
-            children: [
-              Expanded(
-                flex: 2,
-                child: Container(
-                  margin: EdgeInsets.only(
-                    top: 10,
-                    left: 10,
-                    right: 10,
-                  ),
-                  height: 200,
-                  width: 200,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: Colors.blue.shade800,
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Container(
-                  margin: EdgeInsets.only(
-                    top: 10,
-                    left: 10,
-                    right: 10,
-                  ),
-                  height: 200,
-                  width: 200,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: Colors.blue.shade800,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              margin: EdgeInsets.all(10),
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                color: Colors.blue.shade800,
-              ),
-            ),
-          ),
-          Row(
-            children: [
-              for (int i = 0; i < 2; i++)
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    margin: EdgeInsets.only(
-                      left: 10,
-                      bottom: 10,
-                      right: 10,
-                    ),
-                    height: 200,
-                    width: 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                      color: Colors.blue.shade800,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            for (int i = 0; i < 3; i++)
+              Row(
+                children: [
+                  Expanded(
+                    flex: i % 2 == 0 ? 2 : 1,
+                    child: Container(
+                      margin: EdgeInsets.only(
+                        top: 10,
+                        left: 10,
+                        right: 10,
+                      ),
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Colors.blue.shade800,
+                      ),
                     ),
                   ),
-                ),
-            ],
-          ),
-        ]),
+                  Expanded(
+                    flex: i % 2 != 0 ? 2 : 1,
+                    child: Container(
+                      margin: EdgeInsets.only(
+                        top: 10,
+                        left: 10,
+                        right: 10,
+                      ),
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        color: Colors.blue.shade800,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+          ],
+        ),
       ),
     ),
   );
