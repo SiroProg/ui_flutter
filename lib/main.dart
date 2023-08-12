411c99a7769af6a97d82aa3698aede6db3c694e4
SiroProg <070777sss@gmail.com>
2023-08-12 20:47:21 +0500

    9_UI

--- lib/main.dart
+++ lib/main.dart
@@ -5,104 +5,49 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              'AppName',
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+          backgroundColor: Colors.white,
+        ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              margin: EdgeInsets.only(bottom: 10),
-              color: Colors.black38,
-              height: 200,
-              child: Image(
-                image: NetworkImage(
-                    'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/%D0%9C%D1%8B%D1%88%D1%8C_2.jpg/1200px-%D0%9C%D1%8B%D1%88%D1%8C_2.jpg'),
-                fit: BoxFit.cover,
-              ),
-            ),
-            Text(
-              '      Visitors',
-              style: TextStyle(fontWeight: FontWeight.w900),
-            ),
-            Stack(
-              children: [
-                Container(
-                  margin: EdgeInsets.all(10),
-                  width: 80,
-                  height: 80,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.deepOrangeAccent.shade200,
-                  ),
-                  child: Icon(
-                    Icons.person,
-                    size: 40,
-                    color: Colors.deepOrange.shade900,
-                  ),
+            for (int i = 0; i < 5; i++)
+              Container(
+                child: Row(
+                  children: [
+                    Container(
+                      child: Icon(
+                        Icons.image,
+                        size: 50,
+                      ),
+                      color: Colors.black26,
+                      width: 100,
+                      height: 100,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '   Restaurent Name',
+                          style: TextStyle(fontWeight: FontWeight.w900),
+                        ),
+                        Text('    Address'),
+                      ],
+                    )
+                  ],
                 ),
-                Container(
-                  margin: EdgeInsets.only(left: 70, top: 10),
-                  width: 80,
-                  height: 80,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: const Color.fromARGB(255, 64, 214, 255),
-                  ),
-                  child: Icon(
-                    Icons.person,
-                    size: 40,
-                    color: Color.fromARGB(255, 9, 147, 108),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 130, top: 10),
-                  width: 80,
-                  height: 80,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Color.fromARGB(255, 130, 125, 227),
-                  ),
-                  child: Icon(
-                    Icons.person,
-                    size: 40,
-                    color: Color.fromARGB(255, 16, 69, 100),
-                  ),
-                )
-              ],
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 40),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      Icon(
-                        Icons.phone,
-                        size: 30,
-                      ),
-                      Text('Call'),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(
-                        Icons.assistant_navigation,
-                        size: 30,
-                      ),
-                      Text('Route'),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Icon(
-                        Icons.share,
-                        size: 30,
-                      ),
-                      Text('Route'),
-                    ],
-                  ),
-                ],
+                height: 100,
+                margin: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black26, width: 1)),
               ),
-            ),
           ],
         ),
       ),
