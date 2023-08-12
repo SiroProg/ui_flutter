5866998033443c7c0282601af396c724e4f9b4b3
SiroProg <070777sss@gmail.com>
2023-08-12 20:29:28 +0500

    8_UI

--- lib/main.dart
+++ lib/main.dart
@@ -5,73 +5,105 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Expanded(
-            child: Stack(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              color: Colors.black38,
+              height: 200,
+              child: Image(
+                image: NetworkImage(
+                    'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/%D0%9C%D1%8B%D1%88%D1%8C_2.jpg/1200px-%D0%9C%D1%8B%D1%88%D1%8C_2.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            Text(
+              '      Visitors',
+              style: TextStyle(fontWeight: FontWeight.w900),
+            ),
+            Stack(
               children: [
                 Container(
                   margin: EdgeInsets.all(10),
-                  alignment: Alignment.center,
-                  width: 90,
-                  height: 90,
+                  width: 80,
+                  height: 80,
                   decoration: BoxDecoration(
-                    color: Colors.deepOrangeAccent.shade100,
                     shape: BoxShape.circle,
+                    color: Colors.deepOrangeAccent.shade200,
                   ),
                   child: Icon(
                     Icons.person,
-                    color: Color.fromARGB(255, 226, 102, 64),
-                    size: 50,
+                    size: 40,
+                    color: Colors.deepOrange.shade900,
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.only(left: 10, top: 70),
-                  alignment: Alignment.center,
-                  width: 90,
-                  height: 90,
+                  margin: EdgeInsets.only(left: 70, top: 10),
+                  width: 80,
+                  height: 80,
                   decoration: BoxDecoration(
-                    color: Color.fromARGB(215, 0, 217, 255),
                     shape: BoxShape.circle,
+                    color: const Color.fromARGB(255, 64, 214, 255),
                   ),
                   child: Icon(
                     Icons.person,
-                    size: 50,
-                    color: Color.fromARGB(255, 14, 92, 133),
+                    size: 40,
+                    color: Color.fromARGB(255, 9, 147, 108),
                   ),
                 ),
                 Container(
-                  margin: EdgeInsets.only(left: 10, top: 130),
-                  alignment: Alignment.center,
-                  width: 90,
-                  height: 90,
+                  margin: EdgeInsets.only(left: 130, top: 10),
+                  width: 80,
+                  height: 80,
                   decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 255, 177, 100),
                     shape: BoxShape.circle,
+                    color: Color.fromARGB(255, 130, 125, 227),
                   ),
                   child: Icon(
                     Icons.person,
-                    color: Color.fromARGB(255, 194, 138, 7),
-                    size: 50,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(left: 10, top: 190),
-                  alignment: Alignment.center,
-                  width: 90,
-                  height: 90,
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 185, 113, 199),
-                    shape: BoxShape.circle,
-                  ),
-                  child: Icon(
-                    Icons.person,
-                    size: 50,
-                    color: Color.fromARGB(255, 154, 15, 172),
+                    size: 40,
+                    color: Color.fromARGB(255, 16, 69, 100),
                   ),
                 )
               ],
             ),
-          ),
+            Container(
+              margin: EdgeInsets.only(top: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        size: 30,
+                      ),
+                      Text('Call'),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.assistant_navigation,
+                        size: 30,
+                      ),
+                      Text('Route'),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(
+                        Icons.share,
+                        size: 30,
+                      ),
+                      Text('Route'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     ),
