ca703dd2e3121050be601376a396f4b2fd9c7938
SiroProg <070777sss@gmail.com>
2023-08-12 12:52:22 +0500

    Grid_11

--- lib/main.dart
+++ lib/main.dart
@@ -5,28 +5,36 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          children: [
-            for (int i = 0; i < 6; i++)
-              Expanded(
-                child: Row(
-                  children: [
-                    for (int i = 0; i < 4; i++)
-                      Expanded(
-                        child: Container(
-                          margin: EdgeInsets.all(5),
-                          width: 50,
-                          decoration: BoxDecoration(
-                            color: Colors.blue.shade500,
-                            borderRadius: BorderRadius.all(Radius.circular(10)),
-                          ),
-                        ),
-                      ),
-                  ],
-                ),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              margin: EdgeInsets.all(10),
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Colors.blue.shade800,
               ),
-          ],
-        ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: EdgeInsets.only(
+                left: 10,
+                bottom: 10,
+                right: 10,
+              ),
+              height: 100,
+              width: 200,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: Colors.blue.shade800,
+              ),
+            ),
+          )
+        ]),
       ),
     ),
   );
